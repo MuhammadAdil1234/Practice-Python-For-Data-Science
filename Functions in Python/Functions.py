9112e872f606822afa018a12fccacbae0f905e79
Muhammad Adil <muhammadadil0994@gmail.com>
2023-12-07 09:34:15 -0800

    function in python

---
+++ Functions in Python/Functions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# functions
+def first_func ():
+    print('Muhammad Adil')
+first_func()
+
+
+# In[4]:
+
+
+def first_func (str):
+    print('Muhammad Adil {}'.format(str))
+first_func('Badshah')
+
+
+# In[7]:
+
+
+#function with input and return something
+def double_func (num):
+    """"this is the function with string 
+    comment as l make long
+    we can write the detail for anthing to qoute like this"""
+    return num*2
+num = 2
+double = double_func(num)
+print(double)
+
+
+# In[ ]:
+
+
+
+
